414fbf8542fcfee35b0db30153266a1c7ea6a887
Szymon Samuel Zborowski <szzborowski@gmail.com>
2019-09-25 14:05:04 +0200

    Started INI support

---
+++ SongsOrganizer-WinForms/INIFilesReader/INIFile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INIFilesReader
+{
+    public class INIFile
+    {
+        private string Path;
+
+        public INIFile(string nPath)
+        {
+            Path = nPath;
+        }
+
+        public Song Deserialize()
+        {
+            return 
+        }
+
+        public void Serialize()
+        {
+            INISerializer.Serialize(Path);
+        }
+    }
+}
